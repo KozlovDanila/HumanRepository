8d759d89d3a73539966f33f9ef6fb7a8a87fbaad
Danila Kozlov <danila.kozlov23@yandex.ru>
2022-09-28 20:02:27 +0300

    Сделано первое задание

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Human human1 = new Human();
+		System.out.println(human1);
+
+		Human human2 = new Human("Данила", "К", "Д", 26);
+		System.out.println(human2);
+	}
+}
